63fe1ea74b64ab136962299f368f3cd63b4dc8af
Bruno de Santana <bruno.sant.a@gmail.com>
2023-01-12 14:56:38 -0500

    doc: comments functions PWM

--- src/DriverPWM.c
+++ src/DriverPWM.c
@@ -1,10 +1,37 @@
 #include "DriverPWM.h"
 
+/****************************************************************************
+ * Public Functions
+ ****************************************************************************/
+/****************************************************************************
+* Name: PWM_exec
+* 
+* Description:
+*   Execute call to I2C_ic function and set the PWM frequency.
+* Input:
+*   type uint8_t, value 0..100
+*
+* Output:
+*   Negative for error.
+*
+* ***************************************************************************/
 int PWM_exec(uint8_t value) {
     int resp = PWM_OUT(FREQUENCY_10kHz, value);
     return resp;
 }
-
+/****************************************************************************
+* Name: PWM_OUT
+* 
+* Description:
+*   Execute a representation of the board.
+* Input:
+*   uint32_t freq -> PWM Frequency
+*   uint16_t duty -> Duty cycle
+*
+* Output:
+*
+*
+* ***************************************************************************/
 int PWM_OUT(uint32_t freq, uint8_t duty) {
     printf("PWM:%d:%d\n", freq, duty);
     return 0;
